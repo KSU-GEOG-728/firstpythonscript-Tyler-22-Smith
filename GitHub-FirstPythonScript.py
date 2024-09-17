732b3d013187d5088d7af04e066750f00535d738
Tyler-22-Smith <tylersmith@ksu.edu>
2024-09-17 18:44:40 -0500

    Complete
    Requires input via the terminal, final output using "Flint Hills" commented at bottom

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -6,34 +6,57 @@
     Author: Tyler Smith
     Description:  ARC GIS tool to calculate stream area in a selected eco region
     Date created: 09/12/2024
-    Python Version: 3.9.16
+    Python Version: 3.11.8
 """
 
 # Import arcpy module and allow overwrites
 import arcpy
 arcpy.env.overwriteOutput = True
 
-# Set current workspace
-#arcpy.env.workspace = "c:/Users/Tyler Smith/Documents/GitHub/firstpythonscript-Tyler-22-Smith/GitHub-FirstPythonScript.gdb"
+# Set current workspace (file path to geo database in local repo)
 arcpy.env.workspace = "c:/Users/Tyler Smith/Documents/GitHub/firstpythonscript-Tyler-22-Smith/ExerciseData.gdb"
+
+
 
 # Geoprocessing
 
-# Selecting Appropriate Ecoregion
-select_ecoregion = arcpy.SelectLayerByAttribute_management('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
+# Listing Regions Avalible To Choose From
+regions_array = arcpy.da.TableToNumPyArray('ks_ecoregions', ['US_L3NAME'])
+regions_list = regions_array['US_L3NAME']
+print("Select an Ecoregion:")
+ecoregion = input(regions_list) #when running type in terminal after list[]
+
+# Concatenation of selected ecoregion from input() function
+input_selection = "US_L3NAME = " + "'" + ecoregion +  "'"
+selected_ecoregion = arcpy.SelectLayerByAttribute_management('ks_ecoregions', 'NEW_SELECTION', input_selection)
 
 # Buffering Selected Ecoregion by 10 Km
-buffer_ecoregion = arcpy.analysis.Buffer(select_ecoregion, 'ecoregion_buffered', '10 Kilometers')
+buffer_ecoregion = arcpy.analysis.Buffer(selected_ecoregion, 'ecoregion_buffered', '10 Kilometers')
 
 # Clipping Rivers Within the Buffered Ecoregion
 clipped_rivers = arcpy.analysis.Clip('ks_major_rivers', buffer_ecoregion, 'clipped_rivers')
-   
-# Selecting the "Shape_Length" Field Within the clipped_rivers Feature Class and Summing Their Lengths
-total_river_length = 0
-with arcpy.da.SearchCursor('clipped_rivers', "Shape_Length") as cursor:
-    for row in cursor:
-        total_river_length = total_river_length + row[0]
 
+# Extracting Answer
+river_array = arcpy.da.TableToNumPyArray(clipped_rivers, ["Shape_Length"])
+answer = str(round(river_array["Shape_Length"].sum(), ndigits = 0))
 
-# Printing Final Answer
-print("Total Length of Rivers in Selected Ecoregion {0}".format(round(total_river_length, 0)))
+# Print Statements for terminal
+print(" ")
+print(" ")
+print("....Selecting Ecoregion")
+print(" ")
+print(" ")
+print("........Buffering Selection")
+print(" ")
+print(" ")
+print("............Clipping Rivers")
+print(" ")
+print(" ")
+print("########################################################")
+print(" ")
+print("Total Length of Rivers in the {0} = {1} ft".format(ecoregion, answer))
+print(" ")
+print("########################################################")
+
+# Final Output when using "Flint Hills": 
+# Total Length of Rivers in the Flint Hills = 812213.0 ft
