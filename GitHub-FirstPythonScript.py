dc87334375066bd2169106e8e9400e4e641d3334
Shawn Hutchinson <shutch@ksu.edu>
2024-09-18 09:19:40 -0500

    Instructor Corrections
    Script ran to completion, but did not generate correct results.
    
    I simplified your code to better reflect topics discussed at this point and removed two of your result objects.  Those result objects aren't needed (or helpful)...yet.

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -16,52 +16,17 @@ arcpy.env.overwriteOutput = True
 # Set current workspace (file path to geo database in local repo)
 arcpy.env.workspace = "c:/Users/Tyler Smith/Documents/GitHub/firstpythonscript-Tyler-22-Smith/ExerciseData.gdb"
 
-
-
 # Geoprocessing
 
-# Listing Regions Avalible To Choose From
-regions_array = arcpy.da.TableToNumPyArray('ks_ecoregions', ['US_L3NAME'])
-regions_list = regions_array['US_L3NAME']
-print("Select an Ecoregion:")
-ecoregion = input(regions_list) #when running type in terminal after list[]
-
 # Concatenation of selected ecoregion from input() function
-input_selection = "US_L3NAME = " + "'" + ecoregion +  "'"
-selected_ecoregion = arcpy.SelectLayerByAttribute_management('ks_ecoregions', 'NEW_SELECTION', input_selection)
+selected_ecoregion = arcpy.SelectLayerByAttribute_management('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
 
 # Buffering Selected Ecoregion by 10 Km
-buffer_ecoregion = arcpy.analysis.Buffer(selected_ecoregion, 'ecoregion_buffered', '10 Kilometers')
+arcpy.analysis.Buffer(selected_ecoregion, 'ecoregion_buffered', '10 Kilometers')
 
 # Clipping Rivers Within the Buffered Ecoregion
-clipped_rivers = arcpy.analysis.Clip('ks_major_rivers', buffer_ecoregion, 'clipped_rivers')
+arcpy.analysis.Clip('ks_major_rivers', 'ecoregion_buffered', 'clipped_rivers')
 
-# Extracting Answer
-river_array = arcpy.da.TableToNumPyArray(clipped_rivers, ["Shape_Length"])
-answer = str(round(river_array["Shape_Length"].sum(), ndigits = 0))
-answer_miles = str(round(0.000621371 * river_array["Shape_Length"].sum(), ndigits = 0))
-
-# Print Statements for terminal
-print(" ")
-print(" ")
-print("....Selecting Ecoregion")
-print(" ")
-print(" ")
-print("........Buffering Selection")
-print(" ")
-print(" ")
-print("............Clipping Rivers")
-print(" ")
-print(" ")
-print("########################################################")
-print("Total Length of Rivers in the {0} = {1} m".format(ecoregion, answer))
-print(" ")
-print("or")
-print("")
-print(answer_miles + " miles")
-print("########################################################")
-
-# Final Output when using "Flint Hills": 
-# Total Length of Rivers in the Flint Hills = 812213.0 m
-# or 
-# 505 miles
+# Compute stream length in miles and sum new length in output table
+arcpy.AddGeometryAttributes_management("clipped_rivers", "LENGTH", "MILES_US")
+arcpy.Statistics_analysis("clipped_rivers", "outStats", [["LENGTH", "SUM"]])
